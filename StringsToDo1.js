924b333beeb3ff975fdd4571917a3526d4dddb38
Jason Rillera <72282306+JasonRillera@users.noreply.github.com>
2021-05-07 16:47:59 -0700

    Add files via upload

---
+++ StringsToDo1.js
@@ -0,0 +1,64 @@
+// REMOVE BLANKS
+
+function removeBlanks(str) {
+    return str.split(" ").join("");
+}
+
+// GET DIGITS
+
+function getDigits(str) {
+    var stringArr = str.split("");
+    var intStr = "";
+    for (var ind = 0; ind < stringArr.length; ind++) {
+        if (stringArr[ind]%1 == 0) {
+            intStr += stringArr[ind];
+        }
+    }
+    return intStr/1;
+}
+
+// ACRONYMS
+
+function acronyms(str) {
+    var strArr = str.split(" ");
+    var retStr = "";
+    for (var ind = 0; ind < strArr.length; ind++) {
+        if (strArr[ind] == false) {
+            continue;
+        } else {
+            retStr += strArr[ind][0].toUpperCase();
+        }
+    }
+    return retStr;
+}
+
+// COUNT NON SPACES
+
+function countNS(str) {
+    var strArr = str.split("");
+    var count = 0;
+    for (var ind = 0; ind < strArr.length; ind++) {
+        if (strArr[ind] == false) {
+            continue;
+        } else {
+            count++;
+        }
+    }
+    return count;
+}
+
+// REMOVE SHORTER STRINGS
+
+function removeShort(strArr, val) {
+    for (var ind = strArr.length-1; ind >= 0; ind--) {
+        if (strArr[ind].length < val) {
+            for (var i = ind; i < strArr.length-1; i++){
+                var temp = strArr[i];
+                strArr[i] = strArr[i+1];
+                strArr[i+1] = temp;
+            }
+            strArr.pop();
+        }
+    }
+    return strArr;
+}
